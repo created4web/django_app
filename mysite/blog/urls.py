78d7567c88d4123d1c6826e6c6001eb993b5d588
wk1980 <wimkik@gmail.com>
2023-11-18 21:48:14 +0100

    Post verwijderen toegevoegd

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_inhoud'),
     path('post/nieuw/', views.post_nieuw, name='post_nieuw'),
+    path('delete/<post_id>', views.post_delete, name='delete'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -28,15 +28,7 @@ def post_nieuw(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
-def post_delite(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_inhoud', pk=post.pk)
-    else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+def post_delete(request, post_id=None):
+    post_to_delete = Post.objects.get(id=post_id)
+    post_to_delete.delete()
+    return render(request, 'blog/post_list.html')
